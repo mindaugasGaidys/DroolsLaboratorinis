0dbfbc15af01e93caa6ddf1c0918027c0f7059e9
mindaugasGaidys <mindaugas.gaidys@stud.vilniustech.lt>
2022-03-08 15:35:51 +0200

    3rd rule added

--- converter/Converter.java
+++ converter/Converter.java
@@ -1,20 +1,27 @@
 package org.drools.examples.converter;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 import org.antlr.runtime.EarlyExitException;
 import org.drools.compiler.shade.org.eclipse.jdt.internal.compiler.ast.ForeachStatement;
 
 public class Converter {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 		int id;
-		double amount;
+		double amount=0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Choose currency to convert:\n1:USD \t2:EUR \t3:YEN \n4:PLN \t5:NOK");
 		id = sc.nextInt()-1;
-		System.out.println("Enter amount to convert");
-		amount =sc.nextDouble();
-		
+		if(id>4||id<0){
+			System.out.print("\033[H\033[2J");
+			System.out.flush();
+			System.out.println("Invalid choice, please enter number from 1 to 5\n");
+			main(args);
+		}else{
+			System.out.println("Enter amount to convert");
+			amount =sc.nextDouble();
+		}
 		Currency[] currencies= {
 		new Currency("USD" , 1,false),
 		new Currency("EUR", 1.09,false),
